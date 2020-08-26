a7cc610a86dd1c405f3a97fb9098e198cc4cdf30
Philipi Carvalho <philipi.altoe@gmail.com>
2020-08-26 16:51:45 -0300

    OrderStringNumber

---
+++ OrderStringWithNumbers/OrderStringNumber.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderStringWithNumbers
+{
+    public class OrderStringNumber : IComparer<string>
+    {
+        const char PAD = '#';
+
+        public int Compare(string stringA, string stringB)
+        {
+            int biggerStringLength = stringA.Length > stringB.Length ? stringA.Length : stringB.Length;
+
+            stringA = stringA.PadRight(biggerStringLength, PAD);
+            stringB = stringB.PadRight(biggerStringLength, PAD);
+
+            char[] arrayCharA = stringA.ToLower().ToCharArray();
+            char[] arrayCharB = stringB.ToLower().ToCharArray();
+
+            int result = 0;
+
+            for (int pos = 0; pos < biggerStringLength; pos++)
+            {
+                if (arrayCharA[pos] == arrayCharB[pos])
+                    continue;
+
+                if (IsNumber(arrayCharB[pos]) && IsLetter(arrayCharA[pos]))
+                {
+                    result = -1;
+                    break;
+                }
+
+                if (IsNumber(arrayCharA[pos]) && IsLetter(arrayCharB[pos]))
+                {
+                    result = 1;
+                    break;
+                }
+
+                if (arrayCharA[pos] > arrayCharB[pos])
+                {
+                    result = 1;
+                    break;
+                }
+
+                if (arrayCharA[pos] < arrayCharB[pos])
+                {
+                    result = -1;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private bool IsLetter(char letter)
+        {
+            if (letter < 48 || letter > 57)
+                return true;
+            return false;
+        }
+
+        private bool IsNumber(char letter)
+        {
+            if (letter >= 48 && letter <= 57)
+                return true;
+            return false;
+        }
+
+    }
+}
---
+++ OrderStringWithNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderStringWithNumbers
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrExample = { "Street 2A", "Street 3", "Street 4", "Street 3C", "Street 12", "Street 1B", "Street 1", "Street 2", "Street 3A1", "Street 3BC", "Street 3A","Street 22C" };
+
+            //Use OrderBy by Linq and call the custom Comparer class
+            arrExample = arrExample.OrderBy(ex => ex, new OrderStringNumber()).ToArray();
+
+            foreach(var ex in arrExample)
+                Console.WriteLine(ex);
+
+            Console.ReadLine();
+		}
+    }
+}
+
